daac1fae4f23598d080df1706637a6f52a89e355
Naveena <navi.upp@gmail.com>
2023-08-18 11:51:47 -0500

    learning if then else

--- 07-check.sh
+++ 07-check.sh
@@ -2,7 +2,8 @@
 Num=$1
 
 if [Num -gt 10]
-echo "Number is greater than 10"
+then
+    echo "Number is greater than 10"
 else
-echo "Number is not greate than 10"
+    echo "Number is not greate than 10"
 fi
