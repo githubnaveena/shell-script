5cb165e59eb3553d42cc3fd38aa3accf3a17a0eb
Naveena <navi.upp@gmail.com>
2023-08-20 18:00:49 -0500

    logfiles

--- 13-logfiles.sh
+++ 13-logfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APP_LOGS_DIR=./app-logs
+APP_LOGS_DIR=/app-logs
 DATE=$(date +%F)
 LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
 FILES_TO_DELETE= $(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
