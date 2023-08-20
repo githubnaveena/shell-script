fcb700bf4321352953f1fcae63aa8d226f2c13d3
Naveena <navi.upp@gmail.com>
2023-08-20 15:17:44 -0500

    loops

--- 12-loopsinstall.sh
+++ 12-loopsinstall.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+R="\e[32m"
+G="\e[31m"
+N="\e[0m"
+
+validate()
+{
+ if [ $1 =ne 0]
+  then
+ echo -e "$R $1 is Failure $N"
+ else
+ echo -e "$G $1 Sucess $N" 
+}
+
+}
 for i in {@}
 do
     sudo yum install $@ -y
+    validate i
 done
