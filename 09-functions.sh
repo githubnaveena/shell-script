4c5fc1279d58ce9550b273f7bc4c3bd90127714a
Naveena <navi.upp@gmail.com>
2023-08-20 12:34:38 -0500

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -3,14 +3,17 @@
 DATE=$(date +%F)
 SCRIPTNAME=$0
 LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 validate()
 {
 if [ $1 -ne 0 ]
 then
- echo "$2 FAILURE"
+ echo "$2 $R FAILURE $N"
  exit 1
  else 
- echo "$2 SUCCESS"
+ echo "$2 $G SUCCESS $N"
  fi
  }
 
