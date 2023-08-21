35cf097d9f5ca7f236c8c5e8f67c8394e9907f39
Naveena <navi.upp@gmail.com>
2023-08-21 11:12:13 -0500

    deletelogfiles

--- 13-deleteoldlogs.sh
+++ 13-deleteoldlogs.sh
@@ -5,7 +5,7 @@ echo "$APP_LOGS_DIR"
 # SCRIPT_NAME=$0
 # LOG_DIR=/home/centos/shell-script-logs
 # LOGFILE=LOG_DIR/$SCRIPT_NAME-$DATE.log
-# FILES_TO_DELETE= $(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
-# echo $FILES_TO_DELETE
+ FILES_TO_DELETE= $(find $APP_LOGS_DIR -name "*.log" -type f -mtime +18)
+ echo $FILES_TO_DELETE
 
 
